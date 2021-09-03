a025bba0aa905c2fa09b8876fe7b513140f8d93a
joao <jgsspinelli92@gmail.com>
2021-09-03 00:15:37 -0300

    pequena alteração

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-dom-e-seletores/aula/seletores-de-elementos.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-dom-e-seletores/aula/seletores-de-elementos.js
@@ -1,12 +1,13 @@
 // Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
 document.getElementById("paragraph").innerText = "Bastardos Inglorios";
-// // Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+// Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
 document.getElementById("second-paragraph").innerText = "The Beatles";
-// // Por fim, recupere o subtítulo e altere-o também.
+// Por fim, recupere o subtítulo e altere-o também.
 document.getElementById("subtitle").innerText = "Banda Favorita";
 document.getElementById("page-title").innerText = "Filme preferido";
 // Recupere os seus parágrafos via código JavaScript, usando a função getElementByClassName;
 document.getElementsByClassName("allParagraphs")[0].style.background = "#229977";
 document.getElementsByClassName("allParagraphs")[0].style.color = "#fff";
 // Recupere o subtítulo e altere a cor dele usando a função getElementsByTagName.
-document.getElementsByTagName("h4")[0].style.color = "blue";
+document.getElementsByTagName("H4")[0].style.color = "blue";
+
