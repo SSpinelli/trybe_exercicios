bef49a035eadc1a980abbe1819f597f2b37e1b3f
joao <jgsspinelli92@gmail.com>
2021-09-02 21:15:08 -0300

    realização dos exercicios das aulas do course

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-dom-e-seletores/aula/o-dom.js
@@ -0,0 +1,8 @@
+// estrutura do DOM 
+// Window: Representa uma janela que contém um elemento DOM, sendo possível acessar o documento que a janela contém através de Window;
+// Location: Representa a localização do objeto a qual ele está associado, iso é, o documento atual;
+// Document: Representa qualquer página da web carregada do navegador e serve como um ponto de entrada para o conteúdo na página da web, sendo assim o document contém todos os documentos HTML;
+// History: Permite a manipulação do histórico da sessão do navegador, ou seja, as páginas visitadas na guia ou quadro em que a página atual está carregada;
+// Element: É a classe base mais geral da qual todos os objetos em um Document herdam, isso é, são tofas as tags que estão em arquivos HTML e se transformam em elementos da árvore DOM;
+// Text: Texto que vai entre os elementos, é todo o conteúdo das tags;
+// Atribute: São todos os atributos que um nó específico possui, como uma class ou id.
