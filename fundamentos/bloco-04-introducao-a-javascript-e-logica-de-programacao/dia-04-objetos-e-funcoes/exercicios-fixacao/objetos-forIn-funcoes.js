5fdc3a5b4ba135aa186908a724caf3b5b3987b41
joao <jgsspinelli92@gmail.com>
2021-09-05 13:29:16 -0300

    terminando os exercicios do dia 4.4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/exercicios-fixacao/objetos-forIn-funcoes.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/exercicios-fixacao/objetos-forIn-funcoes.js
@@ -57,7 +57,6 @@ function maxNumberIndex (numbersArray) {
   }
   return numberIndex
 }
-console.log(maxNumberIndex([2, 3, 6, 7, 10, 1]))
 // Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 function minNumberIndex (numbersArray) {
   let numberIndex = 0;
@@ -68,7 +67,6 @@ function minNumberIndex (numbersArray) {
   }
   return numberIndex
 }
-console.log(minNumberIndex([2, 4, 6, 7, 10, 0, -3]))
 // Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres;
 function biggestName(namesArray) {
   let biggestName = namesArray[0];
@@ -79,7 +77,6 @@ function biggestName(namesArray) {
   }
   return biggestName;
 }
-console.log(biggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 // Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 function mostRepeated(numbersArray) {
   let mostRepeatedNumberCount = 0;
@@ -100,8 +97,20 @@ function mostRepeated(numbersArray) {
   }
   return numbersArray[indexRepeatedNumber];
 }
-console.log(mostRepeated([2, 3, 2, 5, 8, 2, 3]))
 // Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word;
 function verifyTwoStrings(word, ending) {
-
+  return word.endsWith(ending)
 }
+
+function verificaFimPalavra(palavra, fimPalavra) {
+  palavra = palavra.split('');
+  fimPalavra = fimPalavra.split('');
+  controle = true;
+  for (let index = 0; index < fimPalavra.length; index += 1) {
+    if (palavra[palavra.length - fimPalavra.length + index] != fimPalavra[index]) {
+      controle = false;
+    }
+  }
+  return controle;
+}
+
