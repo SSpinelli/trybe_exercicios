70b6ed2bf324407abae5dfb89ef9e9c473883d11
joao <jgsspinelli92@gmail.com>
2021-09-11 15:49:50 -0300

    completa os exercícios do dia 5.3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/aula/eventos.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/aula/eventos.js
@@ -6,30 +6,74 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-function theOnlyTech(event) {
-  const element = document.querySelector('.tech');
-  element.classList.remove('tech');
+
+// FORMA QUE EU FIZ (MUITO MAL OTIMIZADA, mas FUNCIONAL)
+// const arrayLi = document.querySelectorAll('li')
+
+// firstLi.addEventListener('click', lalala)
+// secondLi.addEventListener('click', lalala2)
+// thirdLi.addEventListener('click', lalala3)
+
+// function lalala () {
+//   for (let index = 0; index < arrayLi.length; index += 1) {
+//     if (arrayLi[index].id = 'first-li') {
+//       firstLi.className = 'tech';
+//       secondLi.className = '';
+//       thirdLi.className = '';
+//     }
+//   }
+// }
+// function lalala2 () {
+//   for (let index = 0; index < arrayLi.length; index += 1) {
+//     if (arrayLi[index].id = 'second-li') {
+//       firstLi.className = '';
+//       secondLi.className = 'tech';
+//       thirdLi.className = '';
+//     }
+//   }
+// }
+// function lalala3 () {
+//   for (let index = 0; index < arrayLi.length; index += 1) {
+//     if (arrayLi[index].id = 'third-li') {
+//       firstLi.className = '';
+//       secondLi.className = '';
+//       thirdLi.className = 'tech';
+//     }
+//   }
+// }
+
+
+// resolução da Trybe
+function changeTechClass(event) {
+  const techElement = document.querySelector('.tech');
+  techElement.classList.remove('tech');
   event.target.classList.add('tech');
-  input.value = '';
 }
-firstLi.addEventListener('click', theOnlyTech);
-secondLi.addEventListener('click', theOnlyTech);
-thirdLi.addEventListener('click', theOnlyTech);
+firstLi.addEventListener('click', changeTechClass);
+secondLi.addEventListener('click', changeTechClass);
+thirdLi.addEventListener('click', changeTechClass)
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-function changingText(string) {
-  
-}
-
+input.addEventListener('input', function(event) {
+  document.querySelector('.tech').innerText = event.target.value;
+})
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
-
+const top3 = document.querySelector('h3');
+function goToMyPortfolio() {
+  window.location.href = 'https://sspinelli.github.io/'
+}
+top3.addEventListener('dblclick', goToMyPortfolio);
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+const title = document.querySelector('h1');
+function changeTitleColor() {
+  title.style.color = '#' + (Math.random().toString(16) + "000000").substring(2,8)
+}
+title.addEventListener('mouseover', changeTitleColor);
 
 // Segue abaixo um exemplo do uso de event.target:
-
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
