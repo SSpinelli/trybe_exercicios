73f57ab6a916bf004e0df9adfd7bef8236ec7b6e
joao <jgsspinelli92@gmail.com>
2021-10-08 02:16:55 -0300

    entrega dos exercícios do dia 8.3

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-map-e-filter/aula.js
@@ -0,0 +1,13 @@
+// Junte esses dois arrays em apenas um;
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+const productsWithPrice = (arrayOfProducts, arrayOfPrices) => {
+  result = {};
+  arrayOfProducts.map((product, index) => {
+    return result[product] = arrayOfPrices[index]
+  })
+  return result;
+}
+
+console.log(productsWithPrice(products, prices))
