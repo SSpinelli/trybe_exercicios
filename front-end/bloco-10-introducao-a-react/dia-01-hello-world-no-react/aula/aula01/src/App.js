5d5efd9006c02a8cd4763d7f40d818e6e163965c
joao <jgsspinelli92@gmail.com>
2021-10-29 15:27:46 -0300

    entrega dos exercícios do dia 10.1

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/aula/aula01/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+const textJSX = 'Nóis é brabo maluco';
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+    </>
+  );
+}
+
+export default App;
