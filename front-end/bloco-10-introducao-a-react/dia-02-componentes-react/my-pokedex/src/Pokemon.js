0ba9374398e3df4d0ce96425b8d8379ba5671347
joao <jgsspinelli92@gmail.com>
2021-11-08 11:40:06 -0300

    exercicio pokedex

--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokemon.js
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokemon.js
@@ -7,16 +7,16 @@ console.log(pokemons);
 class Pokemon extends React.Component {
   render() {
     return (
-      <div>
+      <div className="pokemon-container">
         {pokemons.map((pokemon) => (
           <PokemonCard 
+            className={ pokemon.name }
             key={ pokemon.name }
             name={ pokemon.name }
             type={ pokemon.type }
             image= { pokemon.image }
             averageWeight={ pokemon.averageWeight }
-          />
-        ))}
+          />))}
       </div>
     )
   }
--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/PokemonCard.js
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/PokemonCard.js
@@ -4,11 +4,15 @@ class PokemonCard extends React.Component {
   render() {
     const { name, type, averageWeight, image } = this.props
     return (
-      <div>
-        <h2>{ name }</h2>
-        <h4>{ type }</h4>
-        <h4> { `${averageWeight.value} ${averageWeight.measurementUnit}` } </h4>
-        <img src={ image } alt={ name } />
+      <div className="pokemon" id={ name }>
+        <div>
+          <h2>{ name }</h2>
+          <h4>{ type }</h4>
+          <h4> { `${averageWeight.value} ${averageWeight.measurementUnit}` } </h4>
+        </div>
+        <div>
+          <img src={ image } alt={ name } />
+        </div>
       </div>
     )
   }
